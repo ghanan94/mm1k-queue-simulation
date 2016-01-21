aee57d457187bdd3be96a422ce10f8f83020af8d
Ghanan Gowripalan <ghanan94@gmail.com>
2016-01-21 01:06:36 -0500

    Added getOpt so I can change params quicky

--- lab1.cpp
+++ lab1.cpp
@@ -4,23 +4,51 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	if (argc < 3) {
-		printf("LAB1 (ECE358) - Queue Simulation\n\n");
-		printf("    Syntax: %s <T> <K>\n", argv[0]);
-		printf("%8s%-4s %s\n", "", "T:", "Time to run simulation (s)");
-		printf("%8s%-4s %s\n", "", "K:", "Queue size (0 is infinite)");
-		exit(0);
-	}
+	int T = 10000;
+	int K = 0;
+	int LAMDA = 5;
+	int L = 12000;
+	int ALPHA = 5;
+	int C = 1000000;
 
-	const int T = std::stoi(argv[1]);
-	const int K = std::stoi(argv[2]);
-	const int LAMDA = 5;
-	const int L = 12000;
-	const int ALPHA = 5;
-	const int C = 1000000;
-	const int RHO = 0;
+	int c;
+
+
+	while ((c = getopt (argc, argv, "hA:C:K:L:T:Z:")) != -1) {
+		switch (c) {
+			case 'A':
+				ALPHA = std::stoi(optarg);
+				break;
+			case 'C':
+				C = std::stoi(optarg);
+				break;
+			case 'K':
+				K = std::stoi(optarg);
+				break;
+			case 'L':
+				L = std::stoi(optarg);
+				break;
+			case 'T':
+				T = std::stoi(optarg);
+				break;
+			case 'Z':
+				LAMDA = std::stoi(optarg);
+				break;
+			case 'h':
+			case '?':
+				printf("LAB1 (ECE358) - Queue Simulation\n\n");
+				printf("    Parameters:\n");
+				printf("%8s%-4s %s\n", "", "A:", "(ALPHA) Average number of observer events per second");
+				printf("%8s%-4s %s\n", "", "C:", "Transmission rate of the output link in bits per second");
+				printf("%8s%-4s %s\n", "", "K:", "Queue size (0 is infinite)");
+				printf("%8s%-4s %s\n", "", "L:", "Average length of packet in bits");
+				printf("%8s%-4s %s\n", "", "T:", "Time to run simulation (s)");
+				printf("%8s%-4s %s\n", "", "Z:", "(LAMDA) Average number of packets generated/arrived per second");
+				exit(EXIT_SUCCESS);
+		}
+	}
 
 	simulator(false, T, K, LAMDA, L, ALPHA, C);
 
-	return 0;
+	exit(EXIT_SUCCESS);
 }
