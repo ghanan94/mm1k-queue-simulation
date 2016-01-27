9d3dabbe2fa9ab45adec7e26b424490c9d68f8c3
Ghanan Gowripalan <ghanan94@gmail.com>
2016-01-27 10:54:32 -0500

    Run simulation for every step of rho provided

--- lab1.cpp
+++ lab1.cpp
@@ -4,16 +4,27 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	int T = 10000;
-	int K = 0;
-	int LAMDA = 5;
-	int L = 12000;
-	int ALPHA = 5;
-	int C = 1000000;
-	double RHO = 0.0;
-	bool showEachTimeStamp = false;
+	if (argc < 5) {
+		printf("lab1 <K> <RHO_MIN> <RHO_MAX> <RHO_STEP>\n");
+		exit(1);
+	}
 
-	simulator(showEachTimeStamp, T, K, LAMDA, L, ALPHA, C, RHO);
+	const int T = 10000;
+	const int K = std::stoi(argv[1]);
+	const int LAMDA = 5;
+	const int L = 12000;
+	const int ALPHA = 5;
+	const int C = 1000000;
+	const bool showEachTimeStamp = false;
+
+	const double RHO_MIN = std::stod(argv[2]);
+	const double RHO_MAX = std::stod(argv[3]);
+	const double RHO_STEP = std::stod(argv[4]);
+
+
+	for (double RHO = RHO_MIN; RHO <= RHO_MAX; RHO += RHO_STEP) {
+		simulator(showEachTimeStamp, T, K, LAMDA, L, ALPHA, C, RHO);
+	}
 
 	exit(EXIT_SUCCESS);
 }
