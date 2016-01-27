bd76b5ec3067b5afa5a2995ced67b00be883935e
Ghanan Gowripalan <ghanan94@gmail.com>
2016-01-27 10:47:42 -0500

    current lab1.cpp should only be used for debug purposes

---
+++ lab1_debug.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include "simulator.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int T = 10000;
+	int K = 0;
+	int LAMDA = 5;
+	int L = 12000;
+	int ALPHA = 5;
+	int C = 1000000;
+	double RHO = 0.0;
+	bool showEachTimeStamp = false;
+
+	int c;
+
+
+	while ((c = getopt (argc, argv, "shA:C:K:L:R:T:Z:")) != -1) {
+		switch (c) {
+			case 'A':
+				ALPHA = std::stoi(optarg);
+				break;
+			case 'C':
+				C = std::stoi(optarg);
+				break;
+			case 'K':
+				K = std::stoi(optarg);
+				break;
+			case 'L':
+				L = std::stoi(optarg);
+				break;
+			case 'R':
+				RHO = std::stod(optarg);;
+				break;
+			case 'T':
+				T = std::stoi(optarg);
+				break;
+			case 'Z':
+				LAMDA = std::stoi(optarg);
+				break;
+			case 's':
+				showEachTimeStamp = true;
+				break;
+			case 'h':
+			case '?':
+				printf("LAB1 (ECE358) - Queue Simulation\n\n");
+				printf("    Parameters:\n");
+				printf("%8s%-10s %s\n", "", "-A <arg>", "(ALPHA) Average number of observer events per second");
+				printf("%8s%-10s %s\n", "", "-C <arg>", "Transmission rate of the output link in bits per second");
+				printf("%8s%-10s %s\n", "", "-K <arg>", "Queue size (0 is infinite)");
+				printf("%8s%-10s %s\n", "", "-L <arg>", "Average length of packet in bits");
+				printf("%8s%-10s %s\n", "", "-R <arg>", "Utilization of the queue (= L*Z/C) (This will override Z value)");
+				printf("%8s%-10s %s\n", "", "-T <arg>", "Time to run simulation (s)");
+				printf("%8s%-10s %s\n", "", "-Z <arg>", "(LAMDA) Average number of packets generated/arrived per second");
+				printf("%8s%-10s %s\n", "", "-s", "Show timestamps for every event");
+				printf("%8s%-10s %s\n", "", "-h", "Help");
+				printf("\n");
+				exit(EXIT_SUCCESS);
+		}
+	}
+
+	simulator(showEachTimeStamp, T, K, LAMDA, L, ALPHA, C, RHO);
+
+	exit(EXIT_SUCCESS);
+}
--- makefile
+++ makefile
@@ -1,26 +1,38 @@
-TARGET = lab1
-LIBS = -lm
 CC = g++
 CFLAGS = -g -Wall
 OBJECT_DIR = obj
 
 .PHONY: default all clean
 
-default: $(TARGET)
+default: lab1
 all: default
 
 HEADERS = $(wildcard *.h)
 #HEADERS = $(patsubst %.h, %.h, $(wildcard *.h))
-OBJECTS = $(patsubst %.cpp, $(OBJECT_DIR)/%.o, $(wildcard *.cpp))
+#OBJECTS = $(patsubst %.cpp, $(OBJECT_DIR)/%.o, $(wildcard *.cpp))
+LAB1_OBJECTS = $(OBJECT_DIR)/lab1.o $(OBJECT_DIR)/simulator.o
+LAB1_DEBUG_OBJECTS = $(OBJECT_DIR)/lab1_debug.o $(OBJECT_DIR)/simulator.o
+TEXT_EXPONENTIALRV_OBJECTS = $(OBJECT_DIR)/test_exponentialrv.o
+
+$(OBJECT_DIR)/test_exponentialrv.o: test_exponentialrv.c
+	gcc -g -Wall -c $< -o $@
 
 $(OBJECT_DIR)/%.o: %.cpp $(HEADERS)
 	$(CC) $(CFLAGS) -c $< -o $@
 
 .PRECIOUS: $(TARGET) $(OBJECTS)
 
-$(TARGET): $(OBJECTS)
-	$(CC) $(OBJECTS) -Wall $(LIBS) -o $@
+lab1: $(LAB1_OBJECTS)
+	$(CC) $(CFLAGS) $(LAB1_OBJECTS) -o $@
+
+lab1_debug: $(LAB1_DEBUG_OBJECTS)
+	$(CC) $(CFLAGS) $(LAB1_DEBUG_OBJECTS) -o $@
+
+test_exponentialrv: $(TEXT_EXPONENTIALRV_OBJECTS)
+	gcc -g -Wall $(TEXT_EXPONENTIALRV_OBJECTS) -o $@
 
 clean:
 	-rm -f $(OBJECT_DIR)/*.o
-	-rm -f $(TARGET)
+	-rm -f lab1
+	-rm -f lab1_debug
+	-rm -f test_exponentialrv
